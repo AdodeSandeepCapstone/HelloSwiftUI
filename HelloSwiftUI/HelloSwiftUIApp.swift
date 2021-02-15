ec522bd1015cdc874127f1a4f226de840262f24a
Sandeep Adode <sadode1713@conestogac.on.ca>
2021-02-15 15:55:06 -0500

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Student on 2021-02-13.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
